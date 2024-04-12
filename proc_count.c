240d4e1edd10ecd3a7804e27644d8ae8c49c42a4
Zane LaBute <zlabute@g.ucla.edu>
2024-04-12 16:18:21 -0700

    initial commit from local

---
+++ proc_count.c
@@ -0,0 +1,39 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/sched.h>
+
+static struct proc_dir_entry *entry;
+
+static int proc_count(struct seq_file *m, void *v){
+	// TODO: it's all yours
+	struct task_struct *p;
+	int count = 0;
+
+	for_each_process(p) {
+		count++;
+	}
+
+	return count;
+}
+
+static int __init proc_count_init(void)
+{
+	entry = proc_create_single("count", 0, NULL, proc_count);
+	pr_info("proc_count: init\n");
+	return 0;
+}
+
+static void __exit proc_count_exit(void)
+{
+	proc_remove(entry);
+	pr_info("proc_count: exit\n");
+}
+
+module_init(proc_count_init);
+module_exit(proc_count_exit);
+
+MODULE_AUTHOR("Zane LaBute");
+MODULE_DESCRIPTION("CS111 lab0 count proc number");
+MODULE_LICENSE("GPL");
